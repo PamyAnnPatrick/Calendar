c78a01cd5a1e192f614d80db89b5fd08eccee5c0
Pamy Ann Patrick <pamyannpatrick@gmail.com>
2022-12-28 23:38:51 -0500

    initial

---
+++ Calendar_729/MyCalendar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Calendar_729
+{
+    public class MyCalendar
+    {
+        SortedList<int, int> CalenderData = new SortedList<int, int>();
+        public MyCalendar()
+        {
+
+        }
+
+        public bool Book(int start, int end)
+        {
+            try
+            {
+                CalenderData.Add(start, end);
+            }
+            catch
+            {
+                //Same start already exist, as sorted list doesn't allow duplicate.
+                return false;
+            }
+
+            int count = CalenderData.Count;
+            if (count == 1)
+                return true;
+
+            int currentIndex = CalenderData.IndexOfKey(start);
+
+            //checking if start time is lesser than next element's end time in sorted list  
+            //or end is greater that next element's start time in sorted list
+            if ((currentIndex > 0 && start < CalenderData.Values[currentIndex - 1]) ||
+                (currentIndex < (count - 1) && end > CalenderData.Keys[currentIndex + 1]))
+            {
+                CalenderData.Remove(start);
+                return false;
+            }
+
+
+
+            return true;
+        }
+    }
+}
---
+++ Calendar_729/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar_729
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCalendar obj = new MyCalendar();
+            Console.WriteLine(obj.Book(10, 20));
+            Console.WriteLine(obj.Book(15, 25));
+            Console.WriteLine(obj.Book(20, 30));
+        }
+    }
+}
